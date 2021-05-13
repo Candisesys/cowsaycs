bb6d1dadf42e99a1902993c0fbcabba408409958
Candisesys <johnstove482@gmail.com>
2021-05-13 08:20:17 -0400

    Initial commit

---
+++ cowsaycs.py
@@ -0,0 +1,8 @@
+import cowsay
+import random
+import time
+# oh god this is edgy...
+list1 = ["The human race don't deserve rights.", "I fucking wish the human race was dead.", "The Joker was right all along.",  "Dababy isn't a good artist.", "Ever since the human race killed my cow friend, I've always hated them back since."]
+cowsay.cow(random.choice(list1))
+time.sleep(2)
+print("Yeah... I'm sorry.")
